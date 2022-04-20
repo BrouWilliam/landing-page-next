9e134124909e8ae93b2b3235a560390dc9c60863
BrouWilliam <brouwilliam@hotmail.com>
2022-04-19 21:19:54 -0300

    Initialize project

---
+++ .next/server/pages/faq.js
@@ -0,0 +1,46 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(() => {
+var exports = {};
+exports.id = "pages/faq";
+exports.ids = ["pages/faq"];
+exports.modules = {
+
+/***/ "./pages/faq.js":
+/*!**********************!*\
+  !*** ./pages/faq.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ FAQPage)\n/* harmony export */ });\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-dev-runtime */ \"react/jsx-dev-runtime\");\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__);\n\nfunction FAQPage() {\n    return /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(\"div\", {\n        children: [\n            /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(\"h1\", {\n                children: \"FAQ Page\"\n            }, void 0, false, {\n                fileName: \"C:\\\\Users\\\\WISH YOU WERE HERE\\\\Desktop\\\\alura-cases-divulgacao\\\\pages\\\\faq.js\",\n                lineNumber: 4,\n                columnNumber: 13\n            }, this),\n            /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(\"a\", {\n                href: \"/\",\n                children: \"Ir para a Home\"\n            }, void 0, false, {\n                fileName: \"C:\\\\Users\\\\WISH YOU WERE HERE\\\\Desktop\\\\alura-cases-divulgacao\\\\pages\\\\faq.js\",\n                lineNumber: 5,\n                columnNumber: 13\n            }, this)\n        ]\n    }, void 0, true, {\n        fileName: \"C:\\\\Users\\\\WISH YOU WERE HERE\\\\Desktop\\\\alura-cases-divulgacao\\\\pages\\\\faq.js\",\n        lineNumber: 3,\n        columnNumber: 9\n    }, this);\n};\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9wYWdlcy9mYXEuanMuanMiLCJtYXBwaW5ncyI6Ijs7Ozs7O0FBQUE7QUFBZSxTQUFTQSxPQUFPLEdBQUU7SUFDN0IscUJBQ0ksOERBQUNDLEtBQUc7OzBCQUNBLDhEQUFDQyxJQUFFOzBCQUFDLFVBQVE7Ozs7O29CQUFLOzBCQUNqQiw4REFBQ0MsR0FBQztnQkFBQ0MsSUFBSSxFQUFDLEdBQUc7MEJBQUMsZ0JBQWM7Ozs7O29CQUFJOzs7Ozs7WUFDNUIsQ0FFVDtDQUNKIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vYWx1cmEtY2FzZXMtZGl2dWxnYWNhby8uL3BhZ2VzL2ZhcS5qcz9mNDQ4Il0sInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCBkZWZhdWx0IGZ1bmN0aW9uIEZBUVBhZ2UoKXtcclxuICAgIHJldHVybihcclxuICAgICAgICA8ZGl2PlxyXG4gICAgICAgICAgICA8aDE+RkFRIFBhZ2U8L2gxPlxyXG4gICAgICAgICAgICA8YSBocmVmPVwiL1wiPklyIHBhcmEgYSBIb21lPC9hPlxyXG4gICAgICAgIDwvZGl2PlxyXG5cclxuICAgIClcclxufSJdLCJuYW1lcyI6WyJGQVFQYWdlIiwiZGl2IiwiaDEiLCJhIiwiaHJlZiJdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///./pages/faq.js\n");
+
+/***/ }),
+
+/***/ "react/jsx-dev-runtime":
+/*!****************************************!*\
+  !*** external "react/jsx-dev-runtime" ***!
+  \****************************************/
+/***/ ((module) => {
+
+module.exports = require("react/jsx-dev-runtime");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = (__webpack_exec__("./pages/faq.js"));
+module.exports = __webpack_exports__;
+
+})();
---
+++ pages/faq.js
@@ -0,0 +1,9 @@
+export default function FAQPage(){
+    return(
+        <div>
+            <h1>FAQ Page</h1>
+            <a href="/">Ir para a Home</a>
+        </div>
+
+    )
+}
